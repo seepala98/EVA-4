c4f30715f82f409fddbc6fd88de87e011058d8f2
Seepala <seepala2@gmail.com>
2020-03-24 12:14:02 +0530

    Add files via upload

---
+++ PHASE_1/S10/Transform_utils/trainalbumentation.py
@@ -0,0 +1,21 @@
+from albumentations import *
+from albumentations.pytorch import ToTensor
+import numpy as np
+
+class TrainAlbumentation():
+  def __init__(self):
+    self.train_transform = Compose([
+      HorizontalFlip(),
+      Normalize(
+        mean=[0.485,0.456,0.406],
+        std=[0.229,0.224,0.225],
+      ),
+      CoarseDropout(max_holes=1, max_height=6, max_width=6,
+        fill_value=[0.485, 0.456, 0.406], p=0.5),
+      ToTensor()
+    ])
+
+  def __call__(self, img):
+    img = np.array(img)
+    img = self.train_transform(image = img)['image']
+    return img
