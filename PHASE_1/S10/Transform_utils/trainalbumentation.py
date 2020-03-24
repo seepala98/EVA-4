58b87f9e9b1ff8e1d4210f7a953db3cb96ace6a6
Seepala <seepala2@gmail.com>
2020-03-24 19:06:59 +0530

    Update trainalbumentation.py

--- PHASE_1/S10/Transform_utils/trainalbumentation.py
+++ PHASE_1/S10/Transform_utils/trainalbumentation.py
@@ -10,8 +10,8 @@ class TrainAlbumentation():
         mean=[0.485,0.456,0.406],
         std=[0.229,0.224,0.225],
       ),
-      CoarseDropout(max_holes = 1, max_height = 6, max_width = 6, fill_value = [0.485, 0.456, 0.406], p = 0.5),
-      #Cutout(num_holes=1, max_h_size=6, max_w_size=6, fill_value=[0.485, 0.456, 0.406], p=0.5),
+      #CoarseDropout(max_holes = 1, max_height = 6, max_width = 6, fill_value = [0.485, 0.456, 0.406], p = 0.5),
+      Cutout(num_holes=1, max_h_size=6, max_w_size=6, fill_value=[0.485, 0.456, 0.406], p=0.5),
       ToTensor()
     ])
 
