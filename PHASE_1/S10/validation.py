dd220ef33ce3e4458103b1db09e4d9393aced684
Seepala <seepala2@gmail.com>
2020-03-24 16:17:24 +0530

    Update validation.py

--- PHASE_1/S10/validation.py
+++ PHASE_1/S10/validation.py
@@ -6,18 +6,19 @@ def val(model, val_loader, device, criterion, losses, accuracies):
     correct = 0
     val_loss = 0
     with torch.no_grad():
-        pbar1 = tqdm(testloader)
-        for i, (data, target) in enumerate(pbar1):
-           data, target = data.to(device), target.to(device)
-           outputs = model(data)
-           _, predicted = torch.max(outputs.data, 1)
-           loss = criterion(outputs, target)
-           total += target.size(0)
-           correct += (predicted == target).sum().item()
-        
-        print("Test Loss=", loss.cpu().numpy(), "Test Accuracy=",100*correct/total)
-        #pbar1.update(1)  
-        #print('Loss %0.2f Accuracy of the network on the 10000 test images: %0.2f', loss, (100 * correct / total))
-    test_acc = (100 * correct / total)  
+        for data, target in val_loader:
+            img_batch = data  # This is done to keep data in CPU
+            data, target = data.to(device), target.to(device)  # Get samples
+            output = model(data)  # Get trained model output
+            val_loss += criterion(output, target).item()  # Sum up batch loss
+            pred = output.argmax(dim=1, keepdim=False)  # Get the index of the max log-probability
 
-    return test_acc, loss.cpu().numpy()
+            correct += pred.eq(target).sum().item()
+    
+    val_loss /= len(val_loader.dataset)
+    losses.append(val_loss)
+    accuracies.append(100. * correct / len(val_loader.dataset))
+    print(f'\nValidation set: Average loss: {val_loss:.4f}, Accuracy: {correct}/{len(val_loader.dataset)} ({accuracies[-1]:.2f}%)\n')
+    test_acc = (100 * correct / len(val_loader.dataset))  
+
+    return test_acc, val_loss
