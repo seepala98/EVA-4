8eca02febfbc43c1ca5aca24087acc643fc1c469
Seepala <seepala2@gmail.com>
2020-04-11 21:47:51 +0530

    Add files via upload

---
+++ PHASE_1/S11/validation.py
@@ -0,0 +1,24 @@
+import torch
+from tqdm import tqdm
+
+def val(model, val_loader, device, criterion, losses, accuracies):
+    model.eval()
+    correct = 0
+    val_loss = 0
+    with torch.no_grad():
+        for data, target in val_loader:
+            img_batch = data  # This is done to keep data in CPU
+            data, target = data.to(device), target.to(device)  # Get samples
+            output = model(data)  # Get trained model output
+            val_loss += criterion(output, target).item()  # Sum up batch loss
+            pred = output.argmax(dim=1, keepdim=False)  # Get the index of the max log-probability
+
+            correct += pred.eq(target).sum().item()
+    
+    val_loss /= len(val_loader.dataset)
+    losses.append(val_loss)
+    accuracies.append(100. * correct / len(val_loader.dataset))
+    print(f'\nValidation set: Average loss: {val_loss:.4f}, Accuracy: {correct}/{len(val_loader.dataset)} ({accuracies[-1]:.2f}%)\n')
+    test_acc = (100 * correct / len(val_loader.dataset))  
+
+    return test_acc, val_loss
