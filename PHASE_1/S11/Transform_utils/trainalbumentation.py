cefb7d5fdb7a16ce5e9b3203968236834843c063
Seepala <seepala2@gmail.com>
2020-04-11 21:47:05 +0530

    Add files via upload

---
+++ PHASE_1/S11/Transform_utils/trainalbumentation.py
@@ -0,0 +1,26 @@
+from albumentations import Compose, Normalize, HorizontalFlip, Cutout
+#from imgaug.augmenters.arithmetic import CoarseDropout
+from albumentations.pytorch import ToTensor
+import numpy as np
+
+class TrainAlbumentation():
+  def __init__(self):
+    self.train_transform = Compose([
+      HorizontalFlip(),
+      Rotate((-10.0, 10.0)),
+      Normalize(
+        mean=[0.485,0.456,0.406],
+        std=[0.229,0.224,0.225],
+      ),
+      #CoarseDropout(p=(0.02, 0.1), size_px=None, size_percent=None, per_channel=False, min_size=3, seed=None, name=None, random_state='deprecated', deterministic='deprecated')
+      #CoarseDropout(max_holes = 1, max_height = 6, max_width = 6, fill_value = [0.485, 0.456, 0.406], p = 0.5),
+      PadIfNeeded(min_height=40, min_width=40, border_mode=4, always_apply=False,value=0, p=1.0),
+      RandomCrop(height=32, width=32, always_apply=True, p=1.0),
+      Cutout(num_holes=1, max_h_size=6, max_w_size=6, p=0.5),
+      ToTensor(),
+    ])
+
+  def __call__(self, img):
+    img = np.array(img)
+    img = self.train_transform(image = img)['image']
+    return img
---
+++ PHASE_1/S11/Transform_utils/transform_albumentation.py
@@ -0,0 +1,53 @@
+import torch
+import torchvision
+from Transform_utils.trainalbumentation import TrainAlbumentation
+from Transform_utils.testalbumentation import TestAlbumentation
+
+SEED = 1
+
+class Data():
+
+  def __init__(self):
+    self.train_album = TrainAlbumentation()
+    self.test_album = TestAlbumentation()
+
+  def getTrainDataSet(self, train=True):
+    dataset = torchvision.datasets.CIFAR10(root='./data', train=train, download=True, transform=self.train_album)
+    return dataset
+
+  def getTestDataSet(self, train=False):
+    dataset = torchvision.datasets.CIFAR10(root='./data', train=train, download=True, transform=self.test_album)
+    return dataset
+
+  def getDataLoader(self, dataset, batches):
+    # checking CUDA
+    self.cuda = torch.cuda.is_available()
+    # For reproducibility
+    torch.manual_seed(SEED)
+    if self.cuda:
+      torch.cuda.manual_seed(SEED)
+
+    # dataloader arguments - something you'll fetch these from cmdprmt
+    dataloader_args = dict(shuffle=True, batch_size=batches, num_workers=4, pin_memory=True) if self.cuda else dict(shuffle=True, batch_size=64)
+
+    # train dataloader
+    self.dataset_loader = torch.utils.data.DataLoader(dataset, **dataloader_args)
+
+    return self.dataset_loader
+
+    
+  def getGradCamDataLoader(self, dataset):
+  # checking CUDA
+    self.cuda = torch.cuda.is_available()
+    # For reproducibility
+    torch.manual_seed(SEED)
+    if self.cuda:
+      torch.cuda.manual_seed(SEED)
+
+    # dataloader arguments - something you'll fetch these from cmdprmt
+    dataloader_args = dict(shuffle=True, batch_size=1, num_workers=4, pin_memory=True) if self.cuda else dict(shuffle=True, batch_size=1)
+
+    # train dataloader
+    self.dataset_loader = torch.utils.data.DataLoader(dataset, **dataloader_args)
+
+    return self.dataset_loader
