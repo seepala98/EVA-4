4c7e9100c72833eeb67bcd30d39f21624f70002a
Seepala <seepala2@gmail.com>
2020-04-12 07:36:15 +0530

    Update zigzag_plt.py

--- PHASE_1/S11/Transform_utils/zigzag_plt.py
+++ PHASE_1/S11/Transform_utils/zigzag_plt.py
@@ -2,10 +2,10 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 
-def zigzag_plot(num_trianges = 3, min_val = 0, max_val = 1, triangle_base = 2):
-    x = np.linspace(0, num_trianges * triangle_base, num=1000)
-    tan_val = 2*(max_val-min_val)/triangle_base
+def zigzag_plot(num_tri = 3, min_val = 0, max_val = 1, tri_base = 2):
+    x = np.linspace(0, num_tri * tri_base, num=1000)
+    tan_val = 2*(max_val-min_val)/tri_base
     print(tan_val)
     y = [i*tan_val for i in x]
-    y = [abs(i - triangle_base * np.floor((i+triangle_base/2)/triangle_base))* tan_val + min_val for i in x]
+    y = [abs(i - tri_base * np.floor((i+tri_base/2)/tri_base))* tan_val + min_val for i in x]
     plt.plot(x,y)
