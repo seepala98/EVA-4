2f5c7e31c4b04bb68557e0e4f803614e1984f040
Seepala <seepala2@gmail.com>
2020-04-18 17:04:33 +0530

    Update display.py

--- PHASE_1/S12/display.py
+++ PHASE_1/S12/display.py
@@ -8,8 +8,10 @@ from lr_finder import LRFinder
 def imshow(img):
     img = img / 2 + 0.5     # unnormalize
     npimg = img.numpy()
-    plt.imshow(np.transpose(npimg, (1, 2, 0)))
-
+    fig = plt.figure(figsize=(2,2))
+    plt.imshow(np.transpose(npimg, (1, 2, 0)),interpolation='none')
+    plt.title(c)
+    
 def lr_finder(model, optimizer, criterion, trainloader):
   lr_finder = LRFinder(model, optimizer, criterion, device="cuda")
   lr_finder.range_test(trainloader, end_lr=100, num_iter=100, step_mode="exp")
@@ -72,3 +74,14 @@ def plot_acc_loss(train_acc, test_acc, trainloss_, testloss_):
   axs[1,1].set_title("Test Loss")
   axs[1,1].set_xlabel("Batch")
   axs[1,1].set_ylabel("Loss")
+
+def show_train_data(dataset, classes):
+
+  # get some random training images
+
+  dataiter = iter(dataset)
+  images, labels = dataiter.next()
+  for i in range(len(classes)):
+    index = [j for j in range(1) if labels[j] == i]
+    if(len(index) > 0):
+      imshow(torchvision.utils.make_grid(images[index],nrow=1,padding=2,scale_each=True),classes[i])
