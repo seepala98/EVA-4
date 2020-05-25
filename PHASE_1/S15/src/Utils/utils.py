35d506be2b6c7c334617de67a3b971e52437b19a
Seepala <seepala2@gmail.com>
2020-05-25 08:27:03 +0530

    Add files via upload

---
+++ PHASE_1/S15/src/Utils/utils.py
@@ -0,0 +1,31 @@
+import torchvision
+import matplotlib.pyplot as plt
+
+
+
+from zipfile import ZipFile
+
+
+
+def extract_zip(filename):
+	try:
+        with ZipFile(filename, 'r') as zip:
+            print("Extracting all the files now")
+            zip.extractall()
+            print("Done!")
+    except:
+        print("THIS FILE DOESNT EXIST")
+
+
+def show(tensors, figsize= (10,10), *args, **kwargs):
+	try:
+		tensors = tensors.detach().cpu()
+	except:
+		pass
+	grid_tensor = torchvision.utils.make_grid(tensors, *args, **kwargs)  
+	grid_image  = grid_tensor.permute(1, 2, 0)
+	plt.figure(figsize = figsize)
+	plt.imshow(grid_image)
+	plt.xticks([])  
+	plt.yticks([])
+	plt.show()
