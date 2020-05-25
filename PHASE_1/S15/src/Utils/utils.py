f524b6cdee9f0e135409b8bd0d1b199936fc3497
Seepala <seepala2@gmail.com>
2020-05-25 14:25:11 +0530

    Update utils.py

--- PHASE_1/S15/src/Utils/utils.py
+++ PHASE_1/S15/src/Utils/utils.py
@@ -18,9 +18,9 @@ def show(tensors, figsize= (10,10), *args, **kwargs):
   except:
     pass
   grid_tensor = torchvision.utils.make_grid(tensors, *args, **kwargs)  
-	grid_image  = grid_tensor.permute(1, 2, 0)
-	plt.figure(figsize = figsize)
-	plt.imshow(grid_image)
-	plt.xticks([])  
-	plt.yticks([])
-	plt.show()
+  grid_image  = grid_tensor.permute(1, 2, 0)
+  plt.figure(figsize = figsize)
+  plt.imshow(grid_image)
+  plt.xticks([])  
+  plt.yticks([])
+  plt.show()
